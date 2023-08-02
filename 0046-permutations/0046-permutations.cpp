e822f91fba099b5f45857533526ee7c80f5ff6b6
amans584 <100590012+amans584@users.noreply.github.com>
2023-08-02 10:16:47 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (46.70%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -3,11 +3,25 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         
         vector<vector<int>> vec;
-        sort(nums.begin(),nums.end());
-        do{
-            vec.push_back(nums);
-        }while(next_permutation(nums.begin(),nums.end()));
+        vector<int> curr = {};
+        backtrack(curr, vec, nums);
         
         return vec;
     }
+    
+    void backtrack(vector<int>& curr, vector<vector<int>>& vec, vector<int>& nums) {
+        
+        if(curr.size() == nums.size()) {
+            vec.push_back(curr);
+            return;
+        }
+        for(auto it : nums) {
+            if(find(curr.begin(), curr.end(), it) == curr.end()) {
+                curr.push_back(it);
+                backtrack(curr, vec, nums);
+                curr.pop_back();
+            }
+         }
+    }
+    
 };
