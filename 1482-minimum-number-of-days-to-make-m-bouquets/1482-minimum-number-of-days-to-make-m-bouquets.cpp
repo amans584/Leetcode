cf74535774898619f5e99a4971319519495f8ff8
amans584 <100590012+amans584@users.noreply.github.com>
2023-08-16 23:05:41 +0530

    Time: 125 ms (95.44%), Space: 66.5 MB (52.68%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
     
-    bool check(int x,vector<int>& bloomDay, int m, int k) {
-        int ans =0, cnt=0;
-        for(int i=0;i<bloomDay.size();++i) {
-            if(bloomDay[i] <= x) {
-                cnt++;
+    bool check(int mid, int m, int k, vector<int> &x, int n) {
+        
+        int no_buq =0;
+        int day =0;
+        
+        for(int i=0;i<n;++i) {
+            
+            if(x[i] <= mid) {
+                day++;
             }
-            else {
-                ans += cnt/k;
-                cnt = 0;
+            else{
+                no_buq += day/k;
+                day=0;
             }
         }
-        ans += (cnt/k);
-        if(ans >= m) return true;
+        no_buq += day/k;
+        if(no_buq >= m) return true;
         else return false;
     }
     
     int minDays(vector<int>& bloomDay, int m, int k) {
         
-         int n = bloomDay.size();
+        int n = bloomDay.size();
+        if(n/m < k) return -1;
         
-        if(n/m<k) return -1;
+        int lo = 0, hi = *max_element(bloomDay.begin(), bloomDay.end());
+        int ans = *max_element(bloomDay.begin(), bloomDay.end());
         
-        int lo = 0, hi = *max_element(bloomDay.begin(),bloomDay.end());
-        int ans = *max_element(bloomDay.begin(),bloomDay.end());
-        while(lo<=hi) {
-            int mid = lo + (hi-lo)/2;
-            if(check(mid, bloomDay,m,k)){
+        while(lo <= hi) {
+            
+            int mid = lo + (hi-lo)/2 ;
+            
+            if(check(mid, m, k, bloomDay, n)) {
                 ans = mid;
                 hi = mid - 1;
+                
             }
             else {
                 lo = mid + 1;
